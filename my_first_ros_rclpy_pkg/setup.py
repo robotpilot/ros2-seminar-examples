09f4b7ed852b66794bf0247950a34a75d2330b49
Pyo <pyo@robotis.com>
2020-10-28 07:09:23 +0900

    Modified the description of pkgs
    Signed-off-by: Pyo <pyo@robotis.com>

--- my_first_ros_rclpy_pkg/setup.py
+++ my_first_ros_rclpy_pkg/setup.py
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description='ROS 2 example packages for the ROS 2 seminar',
+    description='ROS 2 rclpy basic package for the ROS 2 seminar',
     license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
