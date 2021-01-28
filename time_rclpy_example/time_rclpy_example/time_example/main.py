5076533e108670d4351c9baebe2c61601de95d2c
Darby Lim <routiful@gmail.com>
2021-01-28 23:37:49 +0900

    Modified node name and applied try and except

--- time_rclpy_example/time_rclpy_example/time_example/main.py
+++ time_rclpy_example/time_rclpy_example/time_example/main.py
@@ -22,31 +22,34 @@ from std_msgs.msg import Header
 def main(args=None):
     rclpy.init(args=args)
 
-    node = rclpy.create_node('minimal_publisher')
+    node = rclpy.create_node('time_example_node')
     time_publisher = node.create_publisher(Header, 'time', 10)
     msg = Header()
 
     rate = node.create_rate(1.0)
     past = node.get_clock().now()
 
-    while rclpy.ok():
-        now = node.get_clock().now()
-        seconds, nanoseconds = now.seconds_nanoseconds()
-        node.get_logger().info('sec {0} nsec {1}'.format(seconds, nanoseconds))
+    try:
+        while rclpy.ok():
+            now = node.get_clock().now()
+            seconds, nanoseconds = now.seconds_nanoseconds()
+            node.get_logger().info('sec {0} nsec {1}'.format(seconds, nanoseconds))
 
-        if ((now - past).nanoseconds * 1e-9) > 5:
-            node.get_logger().info('Over 5 seconds!')
-            past = node.get_clock().now()
+            if ((now - past).nanoseconds * 1e-9) > 5:
+                node.get_logger().info('Over 5 seconds!')
+                past = node.get_clock().now()
 
-        duration = Duration(seconds = 1, nanoseconds = 0)
-        msg.stamp = (now + duration).to_msg()
-        time_publisher.publish(msg)
+            duration = Duration(seconds = 1, nanoseconds = 0)
+            msg.stamp = (now + duration).to_msg()
+            time_publisher.publish(msg)
 
-        rclpy.spin_once(node)
-        rate.sleep()
-
-    node.destroy_node()
-    rclpy.shutdown()
+            rclpy.spin_once(node)
+            rate.sleep()
+    except KeyboardInterrupt:
+        node.get_logger().info('Keyboard Interrupt (SIGINT)')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
