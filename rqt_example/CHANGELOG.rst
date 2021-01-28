db8d04cad3c909adfcb0bb0cb2a47ac3a5d896b7
Pyo <pyo@robotis.com>
2021-01-29 08:41:19 +0900

    Added rqt_example package
    Signed-off-by: Pyo <pyo@robotis.com>

---
+++ rqt_example/CHANGELOG.rst
@@ -0,0 +1,24 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package rqt_example
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.3.0 (2021-01-29)
+------------------
+* Changed code for new ROS 2 seminar
+* Contributors: Pyo
+
+0.2.1 (2019-08-09)
+------------------
+* Changed text of keypad for O/S's compatibility
+* Changed code for PEP-8 and bumped to new version
+* Contributors: Pyo
+
+0.2.0 (2019-08-01)
+------------------
+* Improved code using new code style guides
+* Contributors: Pyo
+
+0.1.0 (2019-06-18)
+------------------
+* Added examples_rqt as rqt plugin for visualizing messages and services
+* Contributors: Pyo, Darby Lim
---
+++ rqt_example/src/rqt_example/examples.py
@@ -0,0 +1,39 @@
+# Copyright 2021 OROCA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from rqt_gui_py.plugin import Plugin
+
+from rqt_example.examples_widget import ExamplesWidget
+
+
+class Examples(Plugin):
+
+    def __init__(self, context):
+        super(Examples, self).__init__(context)
+        self.setObjectName('Examples')
+        self.widget = ExamplesWidget(context.node)
+        serial_number = context.serial_number()
+        if serial_number > 1:
+            self.widget.setWindowTitle(self.widget.windowTitle() + ' ({0})'.format(serial_number))
+        context.add_widget(self.widget)
+
+    def save_settings(self, plugin_settings, instance_settings):
+        self.widget.save_settings(plugin_settings, instance_settings)
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        self.widget.restore_settings(plugin_settings, instance_settings)
+
+    def shutdown_plugin(self):
+        self.widget.trigger_configuration()
