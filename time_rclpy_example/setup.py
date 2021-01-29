a7535db761fa35fce17e8ffe16d9080c56dc07cc
Darby Lim <routiful@gmail.com>
2021-01-29 12:26:14 +0900

    Applied lints

--- time_rclpy_example/setup.py
+++ time_rclpy_example/setup.py
@@ -8,8 +8,7 @@ setup(
     version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -28,5 +27,9 @@ setup(
     description='ROS 2 rclpy example package for understanding time',
     license='Apache License, Version 2.0',
     tests_require=['pytest'],
-    entry_points={'console_scripts': ['time_example = time_rclpy_example.time_example.main:main']},
+    entry_points={
+        'console_scripts': [
+            'time_example = time_rclpy_example.time_example.main:main',
+        ],
+    },
 )
