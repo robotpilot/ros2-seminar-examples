b06fdce6f5f4631c7aa65183bc77e609c7cbb0de
Pyo <pyo@robotis.com>
2021-03-04 08:24:28 +0900

    Modified rviz and launch file for foxy
    Signed-off-by: Pyo <pyo@robotis.com>

--- testbot_description/launch/testbot.launch.py
+++ testbot_description/launch/testbot.launch.py
@@ -2,13 +2,10 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     rviz_display_config_file = os.path.join(
         get_package_share_directory('testbot_description'),
         'rviz',
@@ -17,28 +14,33 @@ def generate_launch_description():
         get_package_share_directory('testbot_description'),
         'urdf',
         'testbot.urdf')
+    with open(urdf_file, 'r') as infp:
+        robot_description_file = infp.read()
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-          'use_sim_time',
-          default_value='false',
-          description='simulation clock on/off'),
+    ld = LaunchDescription()
 
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            arguments=['-d', rviz_display_config_file],
-            output='screen'),
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[
+            {'use_sim_time': False},
+            {'robot_description': robot_description_file}
+        ],
+        output='screen')
 
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=[urdf_file],
-            output='screen'),
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen')
 
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            output='screen')
-    ])
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_display_config_file],
+        output='screen')
+    
+    ld.add_action(robot_state_publisher)
+    ld.add_action(joint_state_publisher_gui)
+    ld.add_action(rviz2)
+
+    return ld
