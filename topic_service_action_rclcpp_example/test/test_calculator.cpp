928d25ea21db829a3183aa4dbf9292556d2efd99
Darby Lim <routiful@gmail.com>
2021-04-20 16:32:18 +0900

    Added tests

--- topic_service_action_rclcpp_example/test/test_calculator.cpp
+++ topic_service_action_rclcpp_example/test/test_calculator.cpp
@@ -12,11 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <memory>
 #include <string>
+#include <memory>
+
+#include "gtest/gtest.h"
 
 #include "calculator/calculator.hpp"
-#include "gtest/gtest.h"
+#include "rclcpp/rclcpp.hpp"
 #include "msg_srv_action_interface_example/srv/arithmetic_operator.hpp"
 
 
@@ -25,30 +27,91 @@ class TestCalculator : public ::testing::Test
 protected:
   void SetUp() override
   {
-    calculator_ = std::make_unique<Calculator>();
+    int argc = 0;
+    char ** argv = nullptr;
+
+    rclcpp::init(argc, argv);
+
+    calculator_ = std::make_shared<Calculator>();
+
+    rclcpp::spin_some(calculator_);
   }
 
   void TearDown() override
   {
+    rclcpp::shutdown();
   }
 
-  std::unique_ptr<Calculator> calculator_;
+  std::shared_ptr<Calculator> calculator_;
 };
 
-TEST_F(TestCalculator, calculate_given_formula)
+TEST_F(TestCalculator, sum)
 {
   msg_srv_action_interface_example::srv::ArithmeticOperator::Request arithmetic_operator;
   float result = 0.0;
 
   result = this->calculator_->calculate_given_formula(4.0f, 2.0f, arithmetic_operator.PLUS);
-  ASSERT_EQ(result, 6.0f);
-
-
+  ASSERT_TRUE(result == 6.0f);
+  ASSERT_FALSE(result == 2.0f);
 }
 
-TEST(TestString, string_comp)
+TEST_F(TestCalculator, subtract)
 {
+  msg_srv_action_interface_example::srv::ArithmeticOperator::Request arithmetic_operator;
+  float result = 0.0;
 
+  result = this->calculator_->calculate_given_formula(-4.0f, -4.0f, arithmetic_operator.MINUS);
+  ASSERT_EQ(result, 0.0f);
+  ASSERT_LE(result, 0.0f);
+  ASSERT_LT(result, 0.1f);
+}
+
+TEST_F(TestCalculator, multiply)
+{
+  msg_srv_action_interface_example::srv::ArithmeticOperator::Request arithmetic_operator;
+  float result = 0.0;
+
+  result = this->calculator_->calculate_given_formula(-2.0f, 5.0f, arithmetic_operator.MULTIPLY);
+  EXPECT_EQ(result, -10.0f);
+  EXPECT_GE(result, -10.0f);
+  EXPECT_GT(result, -11.0f);
+}
+
+TEST_F(TestCalculator, divide)
+{
+  msg_srv_action_interface_example::srv::ArithmeticOperator::Request arithmetic_operator;
+  float result = 0.0;
+
+  result = this->calculator_->calculate_given_formula(10.0f, 5.0f, arithmetic_operator.DIVISION);
+  ASSERT_EQ(result, 2.0f);
+
+  result = this->calculator_->calculate_given_formula(10.0f, 0.0f, arithmetic_operator.DIVISION);
+  ASSERT_EQ(result, 0.0f) << "Failed to divide";
+}
+
+TEST_F(TestCalculator, not_an_operator)
+{
+  float result = 0.0;
+
+  result = this->calculator_->calculate_given_formula(10.0f, 5.0f, 0);
+  EXPECT_EQ(result, 2.0f) << "Not an operator";
+}
+
+TEST(TestString, c_string)
+{
+  std::string abc = "abc";
+  std::string ABC = "ABC";
+
+  const char * cba = "cba";
+  const char * CBA = "CBA";
+
+  EXPECT_EQ(abc.c_str(), "abc");
+
+  EXPECT_STREQ(abc.c_str(), "abc");
+  EXPECT_STRCASEEQ(abc.c_str(), ABC.c_str());
+
+  EXPECT_STRNE(abc.c_str(), cba);
+  EXPECT_STRCASENE(abc.c_str(), CBA);
 }
 
 
